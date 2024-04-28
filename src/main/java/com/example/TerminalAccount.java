a103cf118efa945e1ad31d2b9d602edad389ac1b
Mateus <mateusff2323@gmail.com>
2024-04-28 10:04:32 -0300

    first commit

---
+++ src/main/java/com/example/TerminalAccount.java
@@ -0,0 +1,24 @@
+package com.example;
+import java.util.Scanner;
+
+public class TerminalAccount {
+    private static Scanner scan = new Scanner(System.in);
+    private static int number;
+    private static String agency;
+    private static String name;
+    private static double balance;
+    
+    public static void main(String[] args) {
+        System.out.print("Enter your agency >> ");
+        agency = scan.next();
+        System.out.print("Enter your agency number >> ");
+        number = scan.nextInt();
+        System.out.print("Enter your name >> ");
+        name = scan.next();
+        System.out.print("Enter your balance >> ");
+        balance = scan.nextDouble();
+        scan.close();
+
+        System.out.printf("Olá, %s! Obrigado por criar uma conta no nosso banco, sua agência é %s, número %d e saldo R$%.2f\n", name, agency, number, balance);
+    }
+}
